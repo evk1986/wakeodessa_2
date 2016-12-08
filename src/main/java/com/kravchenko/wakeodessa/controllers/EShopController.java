c8a39cd015ab9ebd08828dc768f9afc44ad9ab08
EGORSST\Egor <evksst@gmail.com>
2016-12-08 12:30:31 +0200

    order

--- src/main/java/com/kravchenko/wakeodessa/controllers/EShopController.java
+++ src/main/java/com/kravchenko/wakeodessa/controllers/EShopController.java
@@ -122,7 +122,7 @@ public class EShopController {
         order.setUser(user);
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         Date date = new Date();
-        System.out.println(dateFormat.format(date)); //2016/11/16 12:08:43
+        System.out.println(dateFormat.format(date));
         order.setDate(date);
         orderService.save(order);
         System.out.println(order.toString());
